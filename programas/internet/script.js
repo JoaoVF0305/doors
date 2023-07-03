c04a8252a1d99978ce8b602627d2c811ed95a966
JVF0305 <110038603+JoaoVF0305@users.noreply.github.com>
2023-07-02 22:04:22 -0300

    Add files via upload

---
+++ programas/internet/script.js
@@ -0,0 +1,16 @@
+{
+
+/* const door = document.querySelector(".notReady");door.classList.remove("notReady")
+const e = door.classList[1]
+
+xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        door.children[1].lastElementChild.innerHTML = xhr.responseText;
+    }
+}
+xhr.open("GET", "programas/internet/sites/gmail/index.html", true);
+xhr.setRequestHeader("Content-type", "text/html");
+xhr.send(); */
+
+}
